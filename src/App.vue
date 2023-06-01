10b86b348d05994fef6466d5f1abad1e1f38fff2
Thomas Henriksen <thomas-henriksen12@hotmail.com>
2023-06-01 11:14:37 +0200

    hej
    hej

--- src/App.vue
+++ src/App.vue
@@ -8,6 +8,7 @@
 <script>
 import ToDoItems from './components/body/ToDoItems.vue';
 import ToDoHeader from './components/header/ToDoHeader.vue';
+import hej from 'C:/Users/thoma/OneDrive/Dokumenter/GitHub';
 
 export default {
   name: 'App',
